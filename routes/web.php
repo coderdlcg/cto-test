7fdb403603d37ceed37de8421f3d8b182c3cca6f
Andrey Cherepanov <coder.dlc@gmail.com>
2023-03-15 21:57:02 +0700

    Add routes and templates for pages of employees and reports

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/employees', function () {
+    return view('employees');
+})->name('employees');
+
+Route::get('/reports', function () {
+    return view('reports');
+})->name('reports');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
