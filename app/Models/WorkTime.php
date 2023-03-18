299df0fcb11e2ed3d39a37d5c47b5d59eb852f27
Andrey Cherepanov <coder.dlc@gmail.com>
2023-03-18 12:26:42 +0700

    Add model, migration and resources.

---
+++ app/Models/WorkTime.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkTime extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['employee_id', 'status', 'value'];
+
+    public const STATUS = [
+        'start' => 0,
+        'stop' => 1,
+    ];
+}
