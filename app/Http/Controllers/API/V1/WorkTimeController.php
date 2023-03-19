f8ffd93b1af680c18594900acb88f18eb0d9e07f
Andrey Cherepanov <coder.dlc@gmail.com>
2023-03-19 17:02:38 +0700

    Add component l5-swagger for documentation api. Add docs for API methods store and update of worktimes.

--- app/Http/Controllers/API/V1/WorkTimeController.php
+++ app/Http/Controllers/API/V1/WorkTimeController.php
@@ -12,9 +12,56 @@ use Illuminate\Http\Response;
 
 class WorkTimeController extends Controller
 {
-
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *      path="/worktimes",
+     *      tags={"Worktimes"},
+     *      summary="Start worktime",
+     *      description="Start worktime for employee_id",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              @OA\Property(property="employee_id", type="integer", example="12345"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="integer", example="12"),
+     *              @OA\Property(property="employee_id", type="integer", example="12345"),
+     *              @OA\Property(property="status", type="string", example="started"),
+     *              @OA\Property(property="value", type="float", example="null"),
+     *              @OA\Property(property="started_at", type="datetime", example="2023-01-01T00:00:01.000000Z"),
+     *              @OA\Property(property="stopped_at", type="datetime", example="2023-01-01T00:00:01.000000Z"),
+     *          )
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Bad Request"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Employee not found",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Employee not found"),
+     *          )
+     *      )
+     * )
      */
     public function store(WorkTimeRequest $request)
     {
@@ -34,7 +81,55 @@ class WorkTimeController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *      path="/worktimes",
+     *      tags={"Worktimes"},
+     *      summary="Stop worktime project",
+     *      description="Stop worktime for employee_id",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(
+     *              @OA\Property(property="employee_id", type="integer", example="12345"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="id", type="integer", example="12"),
+     *              @OA\Property(property="employee_id", type="integer", example="12345"),
+     *              @OA\Property(property="status", type="string", example="stopped"),
+     *              @OA\Property(property="value", type="float", example="1.5"),
+     *              @OA\Property(property="started_at", type="datetime", example="2023-01-01T00:00:00.000000Z"),
+     *              @OA\Property(property="stopped_at", type="datetime", example="2023-01-01T01:30:00.000000Z"),
+     *          )
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Bad Request"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Unauthenticated"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *     @OA\Response(
+     *          response=404,
+     *          description="Employee not found",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Employee not found"),
+     *          )
+     *      )
+     * )
      */
     public function update(WorkTimeRequest $request)
     {
